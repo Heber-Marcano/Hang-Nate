77e1ab064f68ca3d9e73fec57578483e60973a62
Heber Marcano <Hebelioo@gmail.com>
2023-06-07 15:21:26 -0400

    initial file structure

---
+++ Develop/controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const projectRoutes = require('./gameRoutes');
+
+router.use('/users', userRoutes);
+router.use('/projects', projectRoutes);
+
+module.exports = router;
