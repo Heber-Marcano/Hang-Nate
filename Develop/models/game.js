077e2e2ce9275c037d55b700eaa41ee81bc94b30
Heber Marcano <Hebelioo@gmail.com>
2023-06-07 15:20:12 -0400

    started updating game model

---
+++ Develop/models/game.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Project extends Model {}
+
+Project.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    word: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      
+    },
+    won: {
+      type: DataTypes.BOOLEAN,
+    },
+    date_played: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'hangnate',
+  }
+);
+
+module.exports = Project;
