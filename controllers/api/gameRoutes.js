202b5ca663ab387d764aca0c29af3f355e8beb12
Heber Marcano <Hebelioo@gmail.com>
2023-06-07 15:43:36 -0400

    created post create game route

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Game } = require('../../models');
 const withAuth = require('../../utils/auth');
-
+//this route creates game
+//rec.body needs to include the game word and if they won
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newGame = await Game.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newGame);
   } catch (err) {
     res.status(400).json(err);
   }
